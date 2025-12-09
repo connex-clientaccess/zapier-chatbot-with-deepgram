d8cbca4f8e4dc36d79fff3f6ad373636af4c6d28
connex-clientaccess <clientaccess@connex.digital>
2025-12-09 18:08:25 -0400

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Zapier Chatbot with Deepgram",
+  title: "Zapier Chatbot with Deepgram - Demo",
   description: "Converts text to audio using deepgram",
 };
 
